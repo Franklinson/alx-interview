4739d80f0eecd10630da9a23cdeec1f3b7d4b4b2
Franklinson <franklinhassey@gmail.com>
2023-12-06 21:22:58 +0000

    edit code to comply with PEP8

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Boxes containing keys"""
 
+
 def canUnlockAll(boxes):
     if not boxes or not boxes[0]:
         # If there are no boxes or the first box is empty, return False
@@ -8,9 +9,9 @@ def canUnlockAll(boxes):
 
     n = len(boxes)
     visited = [False] * n
-    visited[0] = True 
-    
-    stack = [0] 
+    visited[0] = True
+
+    stack = [0]
 
     while stack:
         current_box = stack.pop()
